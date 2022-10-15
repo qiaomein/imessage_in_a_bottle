0c960d386442d2fa30c0ad37a28766f87efb1c76
ananyaprak <ananya1prakash@gmail.com>
2022-10-15 14:07:43 -0500

    Update App.js

--- App.js
+++ App.js
@@ -13,10 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffl',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
-// ananya test!
