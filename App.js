4b4c0e5875ae99f0e5fce068c1e7010bda137233
ananyaprak <ananya1prakash@gmail.com>
2022-10-15 15:58:25 -0500

    add home/map screens (wips)

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
+import * as React from 'react';
+import MapView from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+// export default 
+function MapScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.testContainer}>
+      <MapView style={styles.map} />
     </View>
   );
 }
 
+function HomeScreen() {
+  return (
+    <View style={styles.testContainer}>
+      <Text>Hello, world!</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
+  testContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
+
+export default App;
